cee132e82e4d2252c7419f5b9387b9ce1e9d170c
Gustave Triomphe <gustave.triomphe@gmail.com>
2025-10-14 15:32:59 +0200

    evaluation script, returns a dictionnary

--- src/evaluation.py
+++ src/evaluation.py
@@ -12,4 +12,4 @@ def evaluate_models(models: list[str], y_pred: pd.DataFrame, y_true: pd.DataFram
     for model in models:
         for metric in metrics:
             res[model][metric] = metrics[metric](y_true, y_pred)
-    return pd.DataFrame(res)
+    return pd.DataFrame(res) 
